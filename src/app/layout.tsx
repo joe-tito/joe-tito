ced7a2e0edfe2a2346ec8ee5d9b4b1972fa5b81b
Joe Tito <joetito1@gmail.om>
2025-02-09 20:20:50 -0500

    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,11 +24,90 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+    <html lang="en" className="scroll-smooth" suppressHydrationWarning>
+      <head>
+        <meta charSet="utf-8" />
+        <meta
+          name="robots"
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+        />
+        <link rel="canonical" href="https://preline.co/" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <meta
+          name="description"
+          content="Perfect for showcasing professional background in a CV/Resume format, highlighting skills, experiences, and achievements."
+        />
+
+        <meta name="twitter:site" content="@preline" />
+        <meta name="twitter:creator" content="@preline" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Personal Tailwind CSS Template | Preline UI, crafted with Tailwind CSS"
+        />
+        <meta
+          name="twitter:description"
+          content="Perfect for showcasing professional background in a CV/Resume format, highlighting skills, experiences, and achievements."
+        />
+        <meta
+          name="twitter:image"
+          content="https://preline.co/assets/img/og-image.png"
+        />
+
+        <meta property="og:url" content="https://preline.co/" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Preline" />
+        <meta
+          property="og:title"
+          content="Personal Tailwind CSS Template | Preline UI, crafted with Tailwind CSS"
+        />
+        <meta
+          property="og:description"
+          content="Perfect for showcasing professional background in a CV/Resume format, highlighting skills, experiences, and achievements."
+        />
+        <meta
+          property="og:image"
+          content="https://preline.co/assets/img/og-image.png"
+        />
+
+        <title>
+          Personal Tailwind CSS Template | Preline UI, crafted with Tailwind CSS
+        </title>
+
+        <link rel="shortcut icon" href="../../favicon.ico" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+
+        <script>
+          const html = document.querySelector('html'); const isLightOrAuto =
+          localStorage.getItem('hs_theme') === 'light' ||
+          (localStorage.getItem('hs_theme') === 'auto' &&
+          !window.matchMedia('(prefers-color-scheme: dark)').matches); const
+          isDarkOrAuto = localStorage.getItem('hs_theme') === 'dark' ||
+          (localStorage.getItem('hs_theme') === 'auto' &&
+          window.matchMedia('(prefers-color-scheme: dark)').matches); if
+          (isLightOrAuto && html.classList.contains('dark'))
+          html.classList.remove('dark'); else if (isDarkOrAuto &&
+          html.classList.contains('light')) html.classList.remove('light'); else
+          if (isDarkOrAuto && !html.classList.contains('dark'))
+          html.classList.add('dark'); else if (isLightOrAuto &&
+          !html.classList.contains('light')) html.classList.add('light');
+        </script>
+
+        <link
+          rel="stylesheet"
+          href="https://preline.co/assets/css/main.min.css"
+        />
+      </head>
+      <body>
+        <ThemeProvider attribute="class">{children}</ThemeProvider>
       </body>
     </html>
   );
