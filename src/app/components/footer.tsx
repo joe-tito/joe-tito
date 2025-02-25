1eec3ba2f75db1e6ded7593f57985a8d0d428af8
joe-tito <joetito1@gmail.com>
2025-02-25 11:22:24 -0500

    footer updates

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -30,7 +30,7 @@ export default function Footer() {
                 GitHub
               </a>
             </li>
-            <li className="inline-block pe-4 text-xs">
+            <li className="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
               <a
                 className="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400"
                 href="https://bsky.app/profile/joetito.bsky.social"
@@ -39,7 +39,7 @@ export default function Footer() {
                 BlueSky
               </a>
             </li>
-            <li className="inline-block pe-4 text-xs">
+            <li className="inline-block relative pe-4 text-xs last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-1.5 before:-translate-y-1/2 before:size-[3px] before:rounded-full before:bg-gray-400 dark:text-neutral-500 dark:before:bg-neutral-600">
               <a
                 className="text-xs text-gray-500 underline hover:text-gray-800 hover:decoration-2 focus:outline-none focus:decoration-2 dark:text-neutral-500 dark:hover:text-neutral-400"
                 href="https://x.com/joetito"
